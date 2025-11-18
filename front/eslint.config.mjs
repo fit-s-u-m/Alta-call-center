52e061651d2283b1340dfc4d900a210ede1a6e27
fit-s-u-m <nehemiahmaths@gmail.com>
2025-11-18 14:16:45 +0300

    add: eslint

--- front/eslint.config.mjs
+++ front/eslint.config.mjs
@@ -1,18 +1,41 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import antfu from "@antfu/eslint-config";
+import eslintNextPlugin from "@next/eslint-plugin-next";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+export default antfu({
+  nextjs: true,
+  typescript: true,
 
-export default eslintConfig;
+  plugins: {
+    next: eslintNextPlugin,
+  },
+
+  extends: [
+    "plugin:@next/next/core-web-vitals",
+  ],
+
+  stylistic: {
+    indent: 2,
+    quotes: "double",
+    semi: true,
+  },
+
+  rules: {
+    "perfectionist/sort-imports": ["error", { tsconfigRootDir: "." }],
+
+    "no-console": "off",
+
+    "unicorn/filename-case": ["error", {
+      cases: {
+        kebabCase: true,
+        pascalCase: true,
+      },
+      ignore: ["README.md"],
+    }],
+    // strict safety rules
+    "@typescript-eslint/no-floating-promises": "error",
+    "@typescript-eslint/no-misused-promises": "error",
+
+    // allow 'any' when needed
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+});
